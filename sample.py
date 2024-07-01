b9aedc55c29507f082ef81ee9611e76483e6506e
Arjun VM <arjunvmviswanathan@gmail.com>
2024-07-01 20:40:12 +0530

    second change

--- sample.py
+++ sample.py
@@ -2,7 +2,7 @@ def prime_gen():
     num = 2
     while True:
         flag = 0
-        for i in range(2, int(num**0.5)+1):
+        for i in range(2, int(num ** 0.5) + 1):
             if num % i == 0:
                 flag = 1
                 break
@@ -10,6 +10,11 @@ def prime_gen():
             yield num
         num += 1
 
+
 gen = prime_gen()
+res = []
 for i in range(20):
-    print(next(gen), end = ' ')
+    res.append(next(gen))
+
+res_map = list(map(lambda x: x ** 2, res))
+print(res_map)
