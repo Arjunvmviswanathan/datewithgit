fcef748828c6d3723ae7be9a14cecfe8691d7461
Arjun VM <arjunvmviswanathan@gmail.com>
2024-07-02 12:02:59 +0530

    Second file changed

---
+++ other.py
@@ -0,0 +1,14 @@
+def dec_func(func):
+    def inner(x, y):
+        y = x * (y - 1)
+        return func(x,y)
+    return inner
+
+@dec_func
+def add_func(a, b):
+    return a + b
+
+res = add_func(4, 5)
+print(res)
+
+
